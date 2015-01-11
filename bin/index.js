f9d5265c18bc93271010f62dd9bae6f0bd442791
Diana Thayer <garbados@gmail.com>
2015-01-11 00:56:38 -0800

    added argv info

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,22 @@
 #!/usr/bin/env node
 
+var pjson = require('../package.json');
 var Puzzler = require('../lib/puzzler');
-var yargs = require('yargs');
+var yargs = require('yargs')
+            .usage('Usage: $0 [action] [-l, --length] [-m, --max]')
+            .example('$0', 'play a game with a 4-length secret with integers from 1 to 6')
+            .example('$0 watch -l 15 -m 25', 'watch an algorithm reason out a 15-length secret with integers from 1 to 25')
+            .default('length', 4)
+            .alias('l', 'length')
+            .default('max', 6)
+            .alias('m', 'max')
+            .demand(1)
+            .version('v' + pjson.version + '\n', 'version');
 var argv = yargs.argv;
 
 switch (argv._[0]) {
   case 'play':
-    var puzzle = new Puzzler(argv._[1], argv._[2]);
+    var puzzle = new Puzzler(argv.length, argv.max);
     console.log('Now playing with');
     console.log('* ' + puzzle.secret_length + '-character secret');
     console.log('* integers from 1 to ' + puzzle.secret_max);
