66a56052af30ccb5614826c55c0a2606e3aa9260
Diana Thayer <garbados@gmail.com>
2017-05-20 21:19:23 -0700

    move GameForHumans to bin/index.js, because testing prompt is hard

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,89 @@
 #!/usr/bin/env node
 
-var Game = require('../lib').GameForHumans
+var async = require('async')
+var Game = require('../lib').Game
 var pkg = require('../package.json')
+var prompt = require('prompt')
+var Table = require('cli-table2')
 var yargs = require('yargs')
+
+class GameForHumans extends Game {
+  preparePrompt () {
+    prompt.message = ''
+    prompt.delimiter = ''
+    prompt.colors = false
+    prompt.start()
+    this._promptReady = true
+  }
+
+  promptGuess (done) {
+    if (!this._promptReady) this.preparePrompt()
+    prompt
+      .get({
+        properties: {
+          guess: {
+            description: 'Please enter your guess:',
+            message: 'Guesses must be space-delimited sequences with exactly ' + String(this.secretLength) + ' integers between 1 and ' + String(this.numChoices) + '.',
+            required: true,
+            type: 'string',
+            pattern: '^' + this.secret.map(() => { return '[1-' + String(this.numChoices) + ']' }).join(' ') + '$',
+            before: function (rawGuess) {
+              return rawGuess.split(' ').map(function (n) {
+                return parseInt(n)
+              })
+            }
+          }
+        }
+      }, function (err, result) {
+        if (err) {
+          done(err)
+        } else {
+          done(null, result.guess)
+        }
+      })
+  }
+
+  respondToGuess (guess) {
+    var response = this.evaluateGuess(guess)
+    var table = new Table({
+      head: ['Guess', 'Correct Position', 'Correct Number']
+    })
+    table.push([guess.join(' '), response.correctPosition, response.correctNumber])
+    console.log(table.toString())
+  }
+
+  play (done) {
+    var history = []
+    // get each turn's guess
+    console.log('Let\'s play a game...')
+    console.log('I\'m thinking of ' + this.secretLength + ' numbers between 1 and ' + this.numChoices + '.')
+    console.log('Can you guess the sequence?')
+    // TODO all this fucking printing makes me bonkers
+    async.doUntil(this.promptGuess.bind(this), (guess) => {
+      history.push(guess)
+      if (this.isGuessCorrect(guess)) {
+        // print: correct! you win!
+        console.log('That is correct! You win')
+        return true
+      } else if (history.length >= this.numGuesses) {
+        // print: you lose! too many guesses!
+        console.log('You lose! Too many guesses!')
+        console.log('The secret was ' + this.secret.join(' '))
+        return true
+      } else {
+        // print what they got right, wrong, etc
+        this.respondToGuess(guess)
+        return false
+      }
+    }, function (err, results) {
+      if (err) {
+        console.log(err)
+      } else if (done) {
+        done(null, results)
+      }
+    })
+  }
+}
 
 // yarr
 var argv = yargs
@@ -31,7 +112,7 @@ var argv = yargs
   .argv
 
 // LET THE GAMES BEGIN
-var game = new Game({
+var game = new GameForHumans({
   numChoices: argv.m,
   secretLength: argv.l,
   numGuesses: argv.g,
--- test/index.js
+++ test/index.js
@@ -10,12 +10,75 @@ describe([pkg.name, pkg.version].join(' v'), function () {
       this.game = new lib.Game()
     })
 
-    it('should generate a secret with appropriate length and values', function () {
-      assert(this.game._opts.secretLength === this.game.secret.length)
+    it('should generate a secret with appropriate length and values.', function () {
+      assert(this.game.secretLength === this.game.secret.length)
       this.game.secret.forEach((n) => {
         assert(n > 0)
         assert(n <= this.game._opts.numChoices)
       })
+    })
+
+    it('should evaluate guesses properly.', function () {
+      this.game.secret = [2, 1, 2, 3]
+      var guesses = [
+        [1, 1, 1, 1],
+        [2, 2, 1, 4],
+        [2, 1, 2, 3]
+      ]
+      var responses = [
+        { correctPosition: 1, correctNumber: 0 },
+        { correctPosition: 1, correctNumber: 2 },
+        { correctPosition: 4, correctNumber: 0 }
+      ]
+      guesses.forEach((guess, i) => {
+        var response = this.game.evaluateGuess(guess)
+        assert(response.correctPosition === responses[i].correctPosition)
+        assert(response.correctNumber === responses[i].correctNumber)
+      })
+    })
+
+    it('should attempt to run a game, but fail due to abstract `guess()`.', function () {
+      try {
+        this.game.play()
+      } catch (err) {
+        assert(err.message === 'Not implemented.')
+      }
+    })
+  })
+
+  describe('GameForMachines', function () {
+    before(function () {
+      this.game = new lib.GameForMachines()
+    })
+
+    it('should attempt to run a game, but fail due to abstract `guess()`.', function () {
+      try {
+        this.game.play()
+      } catch (err) {
+        assert(err.message === 'Not implemented.')
+      }
+    })
+  })
+
+  describe('Cheater', function () {
+    before(function () {
+      this.game = new lib.Cheater()
+    })
+
+    it('should win on the first try cuz it cheated.', function () {
+      this.game.play()
+      assert(this.game.history.length === 1)
+    })
+  })
+
+  describe('Loser', function () {
+    before(function () {
+      this.game = new lib.Loser()
+    })
+
+    it('should lose the game.', function () {
+      var didItWin = this.game.play()
+      assert(didItWin === false)
     })
   })
 })
